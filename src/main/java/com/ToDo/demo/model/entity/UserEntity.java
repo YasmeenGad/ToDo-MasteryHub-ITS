3c5cc00c956d7c8faa8d7a1d207f087d713dd8ee
YasmeenGad <gadyasmeen335@gmail.com>
2025-09-07 23:19:28 +0300

    feat(auth): create user entity and repo chore: implement custom user details service with basic user authentication

---
+++ src/main/java/com/ToDo/demo/model/entity/UserEntity.java
@@ -0,0 +1,23 @@
+package com.ToDo.demo.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
---
+++ src/main/java/com/ToDo/demo/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.ToDo.demo.security;
+
+import com.ToDo.demo.model.entity.UserEntity;
+import com.ToDo.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService  implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Email not found"));
+        return new User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
+
