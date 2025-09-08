7296c5ff30ce529e86a468422e1c2d407bd5e7a7
YasmeenGad <gadyasmeen335@gmail.com>
2025-09-08 12:21:25 +0300

    feat: create reset password request dto to reset password

---
+++ src/main/java/com/ToDo/demo/model/dto/request/ResetPasswordRequestDto.java
@@ -0,0 +1,21 @@
+package com.ToDo.demo.model.dto.request;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResetPasswordRequestDto {
+
+    @NotBlank(message = "Please Enter the password")
+    private String newPassword;
+
+    @NotBlank(message = "please confirm the password")
+    private String confirmPassword;
+}
