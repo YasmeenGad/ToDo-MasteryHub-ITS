3fff57ec8a28533b3b26b46f2b5072063270c1eb
YasmeenGad <gadyasmeen335@gmail.com>
2025-09-08 12:24:42 +0300

    feat: create reset password response dto

---
+++ src/main/java/com/ToDo/demo/model/dto/response/ResetPasswordResponseDto.java
@@ -0,0 +1,15 @@
+package com.ToDo.demo.model.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResetPasswordResponseDto {
+    private String message;
+}
